7ba05477126be57fc9a5ec402ead1163bc6dc3a9
Rownak Mazumder <rmazumder1033@gmail.com>
2024-07-28 02:46:48 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/navigenai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.navigenai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigenHome extends StatelessWidget {
+  const NavigenHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
